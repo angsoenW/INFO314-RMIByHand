8af9c02ea97341507d3cd9606d8e07370c34d850
angsoenW <91903037+angsoenW@users.noreply.github.com>
2023-05-01 15:25:51 -0700

    123

--- Java/Client.java
+++ Java/Client.java
@@ -1,28 +1,58 @@
+import java.io.*;
+import java.net.*;
+
 public class Client {
+
+    public static final int PORT = 10314;
+    public static final String SERVER_ADDRESS = "localhost";
 
     /**
      * This method name and parameters must remain as-is
      */
     public static int add(int lhs, int rhs) {
-        return -1;
+        return (int) executeRemoteCall("add", lhs, rhs);
     }
+
     /**
      * This method name and parameters must remain as-is
      */
     public static int divide(int num, int denom) {
-        return -1;
+        return (int) executeRemoteCall("divide", num, denom);
     }
+    
     /**
      * This method name and parameters must remain as-is
      */
     public static String echo(String message) {
-        return "";
+        return (String) executeRemoteCall("echo", message);
+
     }
+
+    private static Object executeRemoteCall(String methodName, Object... args) {
+        try (Socket socket = new Socket(SERVER_ADDRESS, PORT);
+             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+            out.writeObject(methodName);
+            for (Object arg : args) {
+                out.writeObject(arg);
+            }
+
+            Object result = in.readObject();
+            if (result instanceof Throwable) {
+                throw (Throwable) result;
+            }
+            return result;
+
+        } catch (IOException | ClassNotFoundException | Throwable e) {
+            System.err.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+
 
     // Do not modify any code below this line
     // --------------------------------------
-    String server = "localhost";
-    public static final int PORT = 10314;
 
     public static void main(String... args) {
         // All of the code below this line must be uncommented
--- Java/Server.java
+++ Java/Server.java
@@ -1,4 +1,56 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+
+    public static final int PORT = 10314;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server is running on port " + PORT);
+
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+                     ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
+
+                    String methodName = (String) in.readObject();
+                    Object result;
+
+                    switch (methodName) {
+                        case "add":
+                            int lhs = in.readInt();
+                            int rhs = in.readInt();
+                            result = add(lhs, rhs);
+                            break;
+                        case "divide":
+                            int num = in.readInt();
+                            int denom = in.readInt();
+                            try {
+                                result = divide(num, denom);
+                            } catch (ArithmeticException e) {
+                                out.writeObject(e);
+                                continue;
+                            }
+                            break;
+                        case "echo":
+                            String message = (String) in.readObject();
+                            result = echo(message);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Invalid method name: " + methodName);
+                    }
+
+                    out.writeObject(result);
+
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     // Do not modify any code below tihs line
     // --------------------------------------
